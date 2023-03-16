493500f01601bf39037430678dd8675e858d4724
kerzers <ahra.ahlame@gmail.com>
2023-03-16 12:42:32 -0700

    Print by using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	char a= "with proper grammar, but the outcome is a piece of art,"
-	printf(%s,a);
+	char str[56]= "with proper grammar, but the outcome is a piece of art,";
+	
+	printf(%s,str);
 	return (0);
 }
