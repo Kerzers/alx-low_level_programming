c0d7d323a2150a5c6c30829e5d0d1b240bb829fd
kerzers <ahra.ahlame@gmail.com>
2023-03-17 15:25:47 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char c = 'a'
+
+	while (i <= 9)
+	{
+	putchar(i);
+	i = i + 1;
+	}
+	while (c <= 'f')
+	{
+	putchar(c);
+	c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
