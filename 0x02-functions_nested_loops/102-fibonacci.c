6da172fdf0b2c5f0594718064104d40a3d64342a
kerzers <ahra.ahlame@gmail.com>
2023-03-22 03:44:55 -0700

    Print the 50 first number of Fibonacci serie

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f1 = 0;
+	int f2 = 1;
+	int i, f;
+
+	for (i == 0; i <= 50; i++)
+	{
+	f = f1 + f2;
+		if (i != 50)
+		printf("%d, ", f);
+		else
+		printf("%d\n", f);	
+	f1 = f2;
+	f2 = f;
+	}
+	return (0);
+}
