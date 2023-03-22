ec9e7d291af1f12b677a2a06d73a4945d4072cf7
kerzers <ahra.ahlame@gmail.com>
2023-03-22 01:08:19 -0700

    Times table function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,27 +14,30 @@ void times_table(void)
 		while (j <= 9)
 		{
 		m = i * j;
+		if (j != 0)
+		{
 			if (m > 9)
 			{
+			_putchar(',');
+			_putchar(' ');
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
 			}
 			else
 			{
-			_putchar(m + '0');
-			}
-			if (j < 9)
-			{
 			_putchar(',');
 			_putchar(' ');
-			}
-			if (((i * (j + 1)) / 10 == 0) && (i != 0))
-			{
 			_putchar(' ');
+			_putchar(m + '0');
 			}
-			j = j + 1;
-				if (j == 10)
-				_putchar('\n');
+		}
+		else
+		{
+			_putchar(m + '0');
+		}
+		j = j + 1;
+		if (j == 10)
+		_putchar('\n');
 		}
 	i = i + 1;
 	}
