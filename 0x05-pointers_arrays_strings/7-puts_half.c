b0758d3c4dc93ed28060e750b0dc548202cdc703
kerzers <ahra.ahlame@gmail.com>
2023-03-28 08:09:52 -0700

    Print the half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - prints half string
+ * @str: a string to cut in half
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int cp = 0;
+	int i;
+
+	while (str[cp] != '\0')
+	{
+	cp++;
+	}
+	
+	if (cp % 2 == 0)
+	{
+		for (i = cp / 2; i <= cp; i++)
+		_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (cp + 1) / 2; i <= cp; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
