77fdd3599c00367872c45d0e05cdaa6f8776db86
kerzers <ahra.ahlame@gmail.com>
2023-03-30 01:17:06 -0700

    Add _strcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,10 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j;
 
-	while (dest[i] != '\0')
-	i++,
+	while (dest[i])
+	{
+		i++;
+	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
