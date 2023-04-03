06b6ec2b1586183cf54181bc582c0079f5abc0ed
kerzers <ahra.ahlame@gmail.com>
2023-04-03 14:10:41 -0700

    Add print_diagsums function

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print the sum of diagonals
+ * @a: square matrix
+ * @size: matrix size
+ * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum1 += a[i * size + i];
+			if (i + j == size - 1)
+				sum2 += a[i * size + (size - 1 -i)];
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
