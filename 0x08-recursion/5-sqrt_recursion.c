e781d71059bf434b311e63406815309da6ad7abe
kerzers <ahra.ahlame@gmail.com>
2023-04-05 10:06:57 -0700

    Add sqrt recursion function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,10 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n ==1)
+	else if (n == 0 || n == 1)
 		return (1);
-	else 
-		return(sqrt_iter (n, 2));
+	else
+		return (sqrt_iter(n, 2));
 }
 /**
  * sqrt_iter - tests if k is the square root of n
