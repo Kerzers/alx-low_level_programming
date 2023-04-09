bf6106bbe1e469c127d31e6a27d98bd18da715de
kerzers <ahra.ahlame@gmail.com>
2023-04-09 07:48:04 -0700

    Add function that prints its program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -6,8 +7,9 @@
  *@argv: argument vector
  *Return: 0 (Success)
  */
-int  __attribute__ ((unused))  main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
