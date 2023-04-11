776ba85d68c79bf1e6d53a6362feb9c87d034439
kerzers <ahra.ahlame@gmail.com>
2023-04-11 06:48:19 -0700

    First commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *create_array - creates an array of chars, initializes it with a specific char
+ *@size: size of an array
+ *@c: char to initilize the array
+ *Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This project concerns malloc and free functions
