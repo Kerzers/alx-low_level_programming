6634e7da514ede816a7854f2b45106ea476d125a
kerzers <ahra.ahlame@gmail.com>
2023-04-11 14:39:19 -0700

    Add alloc grid function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	m = malloc(sizeof(int *) * height);
+	if (m == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 	m[i] = malloc(sizeof(int) * width);
