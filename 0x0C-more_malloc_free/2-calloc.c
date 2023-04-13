0af0d5e723895423f84a7e4447964b8d026a8e11
kerzers <ahra.ahlame@gmail.com>
2023-04-13 13:59:23 -0700

    Add calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 	unsigned int i;
 
-	if ( nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+		*((char *)p + i) = 0;
 	return (p);
 }
