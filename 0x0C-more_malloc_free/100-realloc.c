6c753cb98f3f5b4b63837da10124698e9950de06
kerzers <ahra.ahlame@gmail.com>
2023-04-14 06:29:03 -0700

    Add _realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,12 +14,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr != NULL)
 		same = ptr;
-	else
+/*	else
 	{
 		p = malloc(new_size * sizeof(char));
 		if (p == NULL)
 		return (NULL);
-	}
+	}*/
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
