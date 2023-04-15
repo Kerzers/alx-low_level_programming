716c27d68c5c1d2811335033e7fa98466b851614
kerzers <ahra.ahlame@gmail.com>
2023-04-15 04:11:23 -0700

    Add program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,23 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 		printf("0\n");
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (atoi(argv[i]) >= 0)
-			sum += atoi(argv[i]);
-			else
+			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
-			printf("Error\n");
-			return (1);
+				printf("Error\n");
+				return (1);
 			}
 		}
-	printf("%d\n", sum);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) >= 0)
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
