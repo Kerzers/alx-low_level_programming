33f34f2dafe1206f01fc26189d3010e558ef9fbc
kerzers <ahra.ahlame@gmail.com>
2023-04-17 11:03:31 -0700

    Update files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - defines a dog by theses elemests:
  * @name: dog's name
@@ -12,5 +13,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/*dog_t *new_dog(char *name, float age, char *owner);*/
 
 #endif /* DOG_H */
