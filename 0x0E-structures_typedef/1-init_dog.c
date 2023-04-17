46ab40707ce19c1b7c3a4e905c2350410100419d
kerzers <ahra.ahlame@gmail.com>
2023-04-17 10:51:08 -0700

    Add initilize dog function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: a variable of type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of the dog's owner
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	/*if (d == NULL)
+	*	return (NULL);
+	*/
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
