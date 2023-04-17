fcafe31c6878f5f55523e3ebb6902d8d93ee6c90
kerzers <ahra.ahlame@gmail.com>
2023-04-17 10:22:31 -0700

    Add dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - defines a dog by theses elemests:
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of the dog's owner
+ */
 struct dog
 {
 	char *name;
