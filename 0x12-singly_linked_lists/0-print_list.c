6cb066086defb90bffb2081bdaefa30cd33464ff
kerzers <ahra.ahlame@gmail.com>
2023-04-27 06:57:23 -0700

    Quick fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  * print_list - prints all the elements of a list_t list
  * @h: the list to be printed
  * Return: number of nodes
- *
+ * If str is NULL, print [0] (nil)
  */
 size_t print_list(const list_t *h)
 {
