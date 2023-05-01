f31e61ea8ed1344f4fc3d424085cd8595b2dc797
kerzers <ahra.ahlame@gmail.com>
2023-05-01 02:09:03 -0700

    Add print listint function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: the list to be printed
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
