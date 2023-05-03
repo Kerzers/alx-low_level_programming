54f92f9a523e09e9065fd45d5b40ee3bf256da4f
kerzers <ahra.ahlame@gmail.com>
2023-05-03 02:29:25 -0700

    Fix the case head = NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
+	temp = *head;
 	while (temp)
 	{
 		temp = (*head)->next;
