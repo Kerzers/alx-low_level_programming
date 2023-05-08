b3974bef6c3fbec701426009f6600eba5f5b9ca2
kerzers <ahra.ahlame@gmail.com>
2023-05-08 13:36:25 -0700

    Fix betty style

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 #include <string.h>
 /**
  *append_text_to_file - appends text at the end of the file
- *@filname: name of the file
+ *@filename: name of the file
  *@text_content: the NULL terminated string to add at the end of the file
  *Return: 1 on success and -1 on failure
  *Do not create the file if it does not exist
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
 		nw = write(fd, text_content, strlen(text_content));
 		if (nw == -1 || nw != (ssize_t)strlen(text_content))
