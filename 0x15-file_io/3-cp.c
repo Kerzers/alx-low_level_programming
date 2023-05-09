4b8d1917352cf25be6f2d7c971dabc8d55f62c8e
kerzers <ahra.ahlame@gmail.com>
2023-05-09 12:50:13 -0700

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,36 +11,36 @@ int main(int ac, char **av)
 {
 	int origin, copie, close_org, close_cp;
 	ssize_t r_origin, w_copie;
-	char *buffer = NULL;
+	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+	{	dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	origin = open(av[1], O_RDONLY);
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	r_origin = read(origin, buffer, 1024);
-	if (origin == -1 || r_origin == -1)
+	copie = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (origin == -1)
 	{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	copie = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	w_copie = write(copie, buffer, r_origin);
-	if (copie == -1 || w_copie == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+	if (copie == -1)
+	{	dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
+	}
+	while ((r_origin = read(origin, buffer, 1024)) > 0)
+	{	w_copie = write(copie, buffer, r_origin);
+		if (w_copie == -1 || (r_origin != w_copie))
+		{	dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+	if (r_origin == -1)
+	{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	close_org = close(origin);
 	if (close_org == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", origin);
+	{	dprintf(2, "Error: Can't close fd %d\n", origin);
 		exit(100);
 	}
 	close_cp = close(copie);
