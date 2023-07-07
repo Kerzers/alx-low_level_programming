e3ed94fd004a6e4be6072125614e44e98fabc1c4
kerzers <ahra.ahlame@gmail.com>
2023-07-07 13:29:25 -0700

    fix case list

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,30 +14,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL, *current_node;
 	unsigned long int index;
 
-	if (ht->size == 0 || ht == NULL || !*key)
+	if (ht->size == 0 || ht == NULL || !*key || !key)
 		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
+
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (0);
 	new->next = NULL;
 	new->key = strdup(key);
+	if (!new->key)
+		return (0);
 	new->value = strdup(value);
+	if (!new->value)
+		return (0);
 
 	if (ht->array[index] == NULL)
 	{	ht->array[index] = new;
 		return (1);
 	}
 	current_node = ht->array[index];
-	if (strcmp(current_node->key, key) == 0)
+	while (current_node)
 	{
-		current_node->value = strdup(value);
-		return (1);
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			return (1);
+		}
+		current_node = current_node->next;
 	}
-	else
-	{
-		new->next = current_node;
-		ht->array[index] = new;
-		return (1);
-	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
 }
