21a15d80d141b06736ef8116b0343a7cc4c1e9cc
kerzers <ahra.ahlame@gmail.com>
2023-07-07 13:41:54 -0700

    fix case list

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,12 +11,17 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
+	hash_node_t *current;
 
+	if (!ht || !key || !*key)
+		return (NULL);
 	i = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[i])
+	current = ht->array[i];
+	while (current)
 	{
-		if (strcmp((ht->array[i])->key, key) == 0)
-			return (ht->array[i]->value);
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
 	}
 	return (NULL);
 }
