688bb994adbfb96d2ffe683cc6abda622e564ce5
kerzers <ahra.ahlame@gmail.com>
2023-08-23 05:47:45 -0700

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module of island_perimeter"""
+def island_perimeter(grid):
+    """returns the perimeter of the
+    island described in grid
+    """
+    if len(grid) == 0:
+        return
+    len_col = len(grid[0])
+    len_row = len(grid)
+    if len_row > 100 or len_col > 100:
+        return
+    count = 0
+    for i in range(1, len_row - 1):
+        for j in range(1, len_col - 1):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    count +=1
+                if grid[i + 1][j] == 0:
+                    count +=1
+                if grid[i][j - 1] == 0:
+                    count +=1
+                if grid[i][j + 1] == 0:
+                    count +=1
+    return(count)
