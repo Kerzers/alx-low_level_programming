9b86ef825591d8ff419dc569eac1ce55178a1e37
kerzers <ahra.ahlame@gmail.com>
2023-08-24 02:45:14 -0700

    fix task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,17 +13,12 @@ def island_perimeter(grid):
     if len_row > 100 or len_col > 100:
         return
     count = 0
-    for i in range(1, len_row - 1):
-        for j in range(1, len_col - 1):
+    for i in range(len_row):
+        for j in range(len_col):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
-                    count += 1
-                if grid[i + 1][j] == 0:
-                    count += 1
-                if grid[i][j - 1] == 0:
-                    count += 1
-                if grid[i][j + 1] == 0:
-                    count += 1
-                    i += 1
-                    break
+                count += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    count -= 2
+                if j > 0 and grid[i][j - 1]:
+                    count -= 2
     return(count)
